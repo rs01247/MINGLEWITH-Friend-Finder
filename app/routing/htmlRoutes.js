c4f30f379c566c43d7d4d7e6bc175f1db709b71b
Raymond <rs01247@gmail.com>
2018-07-18 16:15:11 -0700

    dependencies added

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,18 @@
+// ROUTES TO THE HOME AND SURVEY HTML PAGES
+const path = require('path');
+const express = require('express');
+// const app = express();
+
+const router = express.Router();
+
+//ROUTE FOR HOME PAGE
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+})
+
+//ROUTE TO TAKE USER TO SURVEY
+router.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+// DEPENDENCIES
 const express = require('express');
 const path = require('path');
-const pareser = require('body-parser');
+const parser = require('body-parser');
+const routes = require('./app/routing/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//BODY PARSING FOR EXPRESS APP
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+app.use('/', routes);
+
+app.listen(PORT, function (){
+    console.log(`Listening on ${PORT}`);
+});
