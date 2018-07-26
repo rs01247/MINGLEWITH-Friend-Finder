e87c04cca3f548c49900cbd05e64da1fa3ab271e
Raymond <rs01247@gmail.com>
2018-07-25 18:23:41 -0700

    updated declarations

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,18 +18,18 @@ router.get('/api/friends', function (req, res) {
 // POST ROUTE TO SEND SURVEY RESULTS TO FRIENDLIST
 router.post('/api/friends', function (req, res) {
     console.log(req.body.scores);
-    var newFriend = req.body;
-    for (var i = 0; i < newFriend.scores.length; i++) {
+    const newFriend = req.body;
+    for (let i = 0; i < newFriend.scores.length; i++) {
         newFriend.scores[i] = Number(newFriend.scores[i]);
     }
 
-    var friendIndex = 0;
-    var minimumDifference = 40;
+    let friendIndex = 0;
+    let minimumDifference = 40;
 
-    for (var i = 0; i < friends.length; i++) {
-        var totalDifference = 0;
-        for (var j = 0; j < friends[i].scores.length; j++) {
-            var difference = Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+    for (let i = 0; i < friends.length; i++) {
+        let totalDifference = 0;
+        for (let j = 0; j < friends[i].scores.length; j++) {
+            let difference = Math.abs(newFriend.scores[j] - friends[i].scores[j]);
             totalDifference += difference;
         }
 
@@ -43,5 +43,5 @@ router.post('/api/friends', function (req, res) {
     res.json(friends[friendIndex]);
 
 });
-
+// 
 module.exports = router;    
